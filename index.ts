7df2c75e35da081ea60bdb09f03a5a085da8f978
AyeshaJamalKhan <160285070+AyeshaJamalKhan@users.noreply.github.com>
2024-03-26 16:53:30 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+#!  /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "FirstNumber" },
+  { message: "Entre Second Number", type: "number", name: "SecondNumber" },
+  {
+    message: "Select on of the operatorsto perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition","Subtraction","Multiplication","Division"],
+  },
+]);
+//conditional operators
+if ( answer.operator==="Addition"){
+    console.log(answer.FirstNumber+ answer.SecondNumber);
+}else if( answer.operator==="Subtraction"){
+    console.log(answer.FirstNumber-answer.SecondNumber);
+}else if(answer.operator==="Multiplication"){
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}else if(answer.operator==="Division"){
+    console.log(answer.FirstNumber/answer.SecondNumber);
+}else{
+    console.log("please select a valid oprator")
+};
